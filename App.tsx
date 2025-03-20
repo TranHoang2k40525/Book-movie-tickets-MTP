76c1de14b600096115ad04f5569bb8e4e756da46
Hoang <hoangzai2k401@gmail.com>
2025-03-20 08:26:23 +0700

    Update navigation and add Home component; remove LoginScreen and update dependencies

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./HomeScreen";
-import LoginScreen from "./LoginScreen";
+import Home from "./Home";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
