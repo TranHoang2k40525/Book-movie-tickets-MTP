6670bc4182b3efe7f6d40778919688524f23cb56
TranHoang2k40525 <hoangzai2k401@gmail.com>
2025-04-13 16:44:32 +0700

    sửa router

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require("../middleware/authMiddleware");
+
+
 
 // Lấy thông tin tài khoản
-router.post('/get-account', userController.getAccount);
+router.post('/get-account', authMiddleware, userController.getAccount);
 
 // Lấy thông tin khách hàng
-router.post('/get-customer', userController.getCustomer);
+router.post('/get-customer', authMiddleware, userController.getCustomer);
 
 // Cập nhật avatar
-router.post('/update-avatar', userController.updateAvatar);
+router.post('/update-avatar', authMiddleware, userController.updateAvatar);
 
 // Cập nhật thông tin khách hàng
-router.put('/update-customer', userController.updateCustomer);
+router.put('/update-customer', authMiddleware, userController.updateCustomer);
 
 // Xóa tài khoản
-router.delete('/delete-account', userController.deleteAccount);
+router.delete('/delete-account', authMiddleware, userController.deleteAccount);
 
 module.exports = router;
